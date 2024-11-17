b5447465b04073a098ba714a47bb8808d0dce77c
Prasadkadam03 <prasadkadam29503@gmail.com>
2024-11-17 14:15:44 +0530

    install jwt and cors , addes bodyparser

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
 const express = require("express");
+const app = express();
+const mainRouter = require("./routes/index.js")
+const userRouter = require("./routes/user.js")
+const {User} = require("./db")
+
+app.use("/api/v1",mainRouter);
+app.use("/api/v1/user",userRouter);
+
+const Port = 3000;
+app.use(express.json());
 
 
+app.listen(Port);
