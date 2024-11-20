34cdcd489d982e6a75a9347f8fae8f123dc0cfcd
Prasadkadam03 <prasadkadam29503@gmail.com>
2024-11-20 19:28:02 +0530

    added account router

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,11 @@ const cors = require("cors")
 app.use(express.json());
 const mainRouter = require("./routes/index.js")
 const userRouter = require("./routes/user.js")
+const accountRouter = require("./routes/account.js")
 
 app.use("/api/v1",mainRouter);
 app.use("/api/v1/user",userRouter);
+app.use("/api/v1/user",accountRouter);
 app.use(cors());
 
 
