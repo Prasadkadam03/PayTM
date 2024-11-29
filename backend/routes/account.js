8cb57bdbb445235d744cb1db1e33caa7208a1159
Prasadkadam03 <prasadkadam29503@gmail.com>
2024-11-29 15:05:58 +0530

    added transfer router with mongoose session

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {authMiddleware} = require("../middleware");
+const { authMiddleware } = require("../middleware");
 const { Account, User } = require("../db");
 const { default: mongoose } = require("mongoose");
 const router = express.Router();
@@ -19,17 +19,17 @@ router.post("/transfer", authMiddleware, async (req, res) => {
 
     session.startTransaction();
     const { amount, to } = req.body;
-    
-    const existingUser = await User.findOne({ username : req.body.to })
-    
-    if(!existingUser) {
+
+    const existingUser = await User.findOne({ username: req.body.to })
+
+    if (!existingUser) {
         return res.status(411).json({
-            message : "user not found "
+            message: "user not found "
         })
     }
     console.log(existingUser._id.toHexString());
 
-    
+
     // Fetch the accounts within the transaction
     const account = await Account.findOne({ userId: req.userId }).session(session);
 
@@ -54,13 +54,12 @@ router.post("/transfer", authMiddleware, async (req, res) => {
         userId: req.userId
     });
 
-    console.log(print.balance);
 
     // Perform the transfer
     await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
     await Account.updateOne({ userId: existingUser._id }, { $inc: { balance: amount } }).session(session);
 
-    
+
     console.log(print.balance);
     // Commit the transaction
     await session.commitTransaction();
