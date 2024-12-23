6f10c2a812741c58d65b8e2db39b97a3f19ad6db
Prasadkadam03 <prasadkadam29503@gmail.com>
2024-12-23 21:48:10 +0530

    fix minor bugs

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -4,6 +4,8 @@ import { Heading } from "../Components/Heading"
 import { InputBox } from "../Components/InputBox"
 import { useState } from "react"
 import axios from "axios"
+import { toast } from 'react-toastify';
+import { useNavigate } from "react-router-dom"
 
 export const SendMoney = () => {
 
@@ -13,18 +15,21 @@ export const SendMoney = () => {
     const fname = searchParams.get("fname");
     const lname = searchParams.get("lname");
 
-    const [Amount, setAmount] = useState("");
+    const [amount, setAmount] = useState("");
+
+    const navigate = useNavigate();
+
 
 
     return <div className="bg-zinc-300 h-screen flex justify-center">
-        <div className="flex flex-col justify-center w-full max-w-md p-4">
-            <div className="rounded-lg bg-white shadow-2xl shadow-gray-900 hover:shadow-cyan-700/100 w-full p-8">
+        <div className=" flex flex-col justify-center ">
+            <div className="rounded-lg bg-white  shadow-2xl shadow-gray-900 hover:shadow-cyan-700/100 w-110 p-8 ">
                 <div className="flex justify-center">
                     <Heading label={"SEND MONEY"} />
                 </div>
-                <div className="flex justify-start px-4 py-4">
+                <div className=" flex  justify-start px-16 py-4">
                     <div className="rounded-full h-10 w-10 bg-green-400 flex justify-center mt-1 mr-2">
-                        <div className="flex flex-col items-center justify-center text-xl">
+                        <div className="flex flex-col items-center justify-center  text-xl">
                             {fname[0].toUpperCase()}
                         </div>
                     </div>
@@ -32,14 +37,16 @@ export const SendMoney = () => {
                         {fname} {lname}
                     </div>
                 </div>
-                <div className="p-1">
-                    <div className="flex flex-start block p-1.5 text-sm font-medium">
+
+
+                <div className="p-1 ">
+                    <div className=" flex flex-start block p-1.5 text-sm font-medium ">
                         Amount ( in ₹ )
                     </div>
                     <input
                         onChange={(e) => {
                             const value = e.target.value;
-                            if (value >= 0 || value === "") { 
+                            if (value >= 0 || value === "") {
                                 setAmount(value);
                             }
                         }}
@@ -50,21 +57,31 @@ export const SendMoney = () => {
                         className="bg-slate-50 border border-slate-300 text-bold text-medium rounded-lg w-full p-2"
                         required
                     />
+
                 </div>
+
                 <Button onPress={() => {
-                    axios.post(import.meta.env.VITE_SERVER_URL + "/api/v1/account/transfer", {
-                        to: id,
-                        amount: Amount,
-                    }, {
-                        headers: {
-                            Authorization: "Bearer " + localStorage.getItem("token")
-                        }
-                    })
-                    .then((response) => {
-                        alert("Transfer successful");
-                    })
+                    const response = axios.post(
+                        `${import.meta.env.VITE_SERVER_URL}/api/v1/account/transfer`,
+                        { to: id, amount },
+                        { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } }
+                    );
+
+                    toast.promise(
+                        response,
+                        {
+                            pending: "Initiating transfer...",
+                            success: "Transfer initiated successfully",
+                            error: "Failed to initiate transfer",
+                        },
+
+                    )
+
                 }} label={"Initiate transfer"} />
+                <Button onPress={() => {
+                    navigate("/dashboard");
+                }} label={"Dashboard"} />
             </div>
         </div>
     </div>
-}
+} 
