27836963e0c484d311ccb160e9b6883b4333e4b4
Prasadkadam03 <prasadkadam29503@gmail.com>
2024-12-26 09:49:55 +0530

    fix minor bugs

--- frontend/src/Components/Button.jsx
+++ frontend/src/Components/Button.jsx
@@ -1,7 +1,7 @@
 export const Button = ({ label , onPress }) => {
     return (
       <div className="sm:py-2 sm:px-2 py-2 pl-0.5 pr-0">
-        <button onClick={onPress} className="bg-gray-900 hover:bg-gray-800 text-white font-medium px-3 py-2 rounded-lg w-full h-1/2">
+        <button onClick={onPress} className="bg-gray-700 hover:bg-gray-900  text-white font-medium px-3 py-2 rounded-lg w-full h-1/2">
         {label}
       </button>
       </div>
